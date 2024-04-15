9017fa675afc30a9188293cfe9473d1d89873e21
seed-yoo <lukdown10@naver.com>
2024-04-15 14:45:19 +0900

    .

--- src/main/java/com/javaex/config/WebMvcConfig.java
+++ src/main/java/com/javaex/config/WebMvcConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 	
+	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**") // 경로
